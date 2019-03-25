4dde66339582933bd0337e520479b8d3907a7c80
Josef Roth <jr@netsyno.com>
2019-03-25 16:02:25 +0100

    Move peer dependencies to dependencies, update versions and add rules for React Hooks

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react-hooks'],
   rules: {
     'react/jsx-filename-extension': 0,
     'react/prop-types': 0,
     'arrow-parens': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
