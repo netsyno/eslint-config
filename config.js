aaa0cf8d70c8604b0c765023d79c8b19bb5c4492
Josef Roth <jr@netsyno.com>
2019-04-08 10:38:13 +0200

    Integrate linting for typescript

--- config.js
+++ config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  plugins: ['react-hooks'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint', 'react-hooks'],
   rules: {
     'react/jsx-filename-extension': 0,
     'react/prop-types': 0,
     'arrow-parens': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'off',
   },
 };
